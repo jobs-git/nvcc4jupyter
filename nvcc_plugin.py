52d8030027d15b14eee50d3e7dd61e0cfe67de03
Jobs Gate <organicchemistry_01@yahoo.com.ph>
2023-02-21 13:49:30 +0800

    Update nvcc_plugin.py

--- nvcc_plugin.py
+++ nvcc_plugin.py
@@ -2,8 +2,5 @@ from v2.v2 import NVCCPluginV2 as NVCC_V2
 
 
 def load_ipython_extension(ip):
-    nvcc_plugin = NVCC_V1(ip)
-    ip.register_magics(nvcc_plugin)
-
     nvcc_plugin_v2 = NVCC_V2(ip)
     ip.register_magics(nvcc_plugin_v2)
